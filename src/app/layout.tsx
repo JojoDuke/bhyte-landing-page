befd9de4b7fdfc2c9887f8bd17580d17244bc6c8
JojoDuke <jojoamankwa@gmail.com>
2026-03-26 18:22:39 +0000

    lmbfff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import Script from "next/script";
 import { PostHogProvider } from "./providers";
 
 const geistSans = Geist({
